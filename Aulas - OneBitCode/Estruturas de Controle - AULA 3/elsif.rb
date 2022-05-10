50bd001a7e92e6298d48678b3c2afc23a9273265
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-10 18:13:12 -0300

    dev-com-rb-elsif

---
+++ Aulas - OneBitCode/Estruturas de Controle - AULA 3/elsif.rb
@@ -0,0 +1,13 @@
+day = "Holiday"
+
+if day == "Sunday"
+    lunch = "special"
+
+elsif day == "Holiday"
+    lunch = "later"
+
+else
+    lunch = "not special"
+end
+
+puts "Lunch is #{lunch} today!"
