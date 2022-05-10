9fda0ea81a0cceb50bfaa354858b2b6d2f7240fc
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-10 17:16:00 -0300

    dev-com-first_ruby

---
+++ Aulas - OneBitCode/Conceitos Básicos - AULA 2/hello.rb
@@ -0,0 +1,9 @@
+# Imprime dados sem pular uma linha.
+print "Digite o seu nome: "
+
+# Armazena entrada de dados do usuário em uma variável
+name = gets.chomp
+
+# Imprime dados pulando uma linha
+# Uso de variável dentro de uma string
+puts "Hello #{name}"
