d08fa672433e9a6bf763772e8a9fe4f5693e7362
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-10 18:16:45 -0300

    dev-com-rb-unless

---
+++ Aulas - OneBitCode/Estruturas de Controle - AULA 3/unless.rb
@@ -0,0 +1,11 @@
+product_status = "closed"
+
+# "if not" funcionaria do mesmo jeito que "unless"
+unless product_status == "open"
+    check_change = "can"
+
+else
+    check_change = "can not"
+end
+
+puts "You #{check_change} change the product!"
