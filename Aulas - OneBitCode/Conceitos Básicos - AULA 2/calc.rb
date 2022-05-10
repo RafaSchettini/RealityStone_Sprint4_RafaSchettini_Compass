e81a1bc214535e31e01caaecbff0fbad6c3f459a
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-10 17:35:31 -0300

    dev-com-rb-calc

---
+++ Aulas - OneBitCode/Conceitos Básicos - AULA 2/calc.rb
@@ -0,0 +1,9 @@
+print "Digite um número: "
+# to_i transforma uma string em um número inteiro
+num1 = gets.chomp.to_i
+
+print "Digite outro número: "
+num2 = gets.chomp.to_i
+
+sum = num1 + num2
+puts "O resultado da soma entre esses números é igual a: #{sum}"
