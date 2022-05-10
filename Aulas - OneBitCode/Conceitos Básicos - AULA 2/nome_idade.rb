ca06e3fb898a5060f4251fe669e5ae30a7ce4abb
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-10 17:46:31 -0300

    dev-com-rb-aula2desafios

---
+++ Aulas - OneBitCode/Conceitos Básicos - AULA 2/nome_idade.rb
@@ -0,0 +1,7 @@
+print "Digite o seu nome: "
+name = gets.chomp
+
+print "Digite a sua idade: "
+age = gets.chomp.to_i
+
+puts "Você se chama #{name} e possui #{age} anos"
---
+++ Aulas - OneBitCode/Conceitos Básicos - AULA 2/operations.rb
@@ -0,0 +1,17 @@
+print "Digite o primeiro número: "
+num1 = gets.chomp.to_i
+
+print "Digite o segundo número: "
+num2 = gets.chomp.to_i
+
+sum = num1 + num2
+puts "O resultado da soma entre esses números é igual a: #{sum}"
+
+subtraction = num1 - num2
+puts "O resultado da subtração entre esses números é igual a: #{subtraction}"
+
+mult = num1 * num2
+puts "O resultado da multiplicação entre esses números é igual a: #{mult}"
+
+div = num1 / num2
+puts "O resultado da divisão entre esses números é igual a: #{div}"
