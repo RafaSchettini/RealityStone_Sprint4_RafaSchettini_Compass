c9c794a009e52f34239c032413c093485ebc2d10
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-11 15:30:39 -0300

    dev-com-rb-challenge-aula3
    Challenge para criar uma calculador que realize as 4 operações básicas entre dois números digitados pelo usuário.

---
+++ Aulas - OneBitCode/Estruturas de Controle - AULA 3/calculator.rb
@@ -0,0 +1,41 @@
+system "cls"
+
+loop do
+    print "Digite o primeiro número: "
+    num1 = gets.chomp.to_i
+
+    print "Digite o segundo número: "
+    num2 = gets.chomp.to_i
+    
+    puts "Agora escolha uma das operações abaixo:"
+    puts "1 - Adição"
+    puts "2 - Subtração"
+    puts "3 - Multiplicação"
+    puts "4 - Divisão"
+    puts "0 - Fechar o programa."
+
+    choice = gets.chomp.to_i
+
+    if choice == 1
+        sum = num1 + num2
+        puts "O resultado da adição entre #{num1} + #{num2} é igual a: #{sum}"
+    elsif choice == 2
+        subtraction = num1 - num2
+        puts "O resultado da subtração entre #{num1} - #{num2} é igual a: #{subtraction}"
+    elsif choice == 3
+        mult = num1 * num2
+        puts "O resultado da multiplicação entre #{num1} * #{num2} é igual a: #{mult}"
+    elsif choice == 4
+        div = num1 / num2
+        puts "O resultado da divisão entre #{num1} / #{num2} é igual a: #{div}"    
+    elsif choice == 0
+        break
+    else
+        puts "Você digitou uma opção inválida. Tente novamente:"
+    end
+    
+    puts "O programa irá reiniciar em 5 segundos..."
+    sleep(5)
+    system "cls"
+
+end
