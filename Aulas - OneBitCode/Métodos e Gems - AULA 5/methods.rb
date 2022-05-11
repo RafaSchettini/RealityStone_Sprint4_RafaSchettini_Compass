3756f057daa6f96bbf3ae9e456dc58e58cb0ed52
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-11 19:25:15 -0300

    dev-com-rb-methods

---
+++ Aulas - OneBitCode/Métodos e Gems - AULA 5/methods.rb
@@ -0,0 +1,17 @@
+# Parâmetros necessários pois não recebem um valor inicialmente
+def talk(first_name, last_name)
+    puts "Olá #{first_name} #{last_name}!"
+end
+
+talk("Rafael", "Schettini")
+
+# Parâmetro opcional pois já recebe um valor
+def signal(color = "vermelho")
+    puts "O sinal está #{color}"
+end
+
+# Chamando método sem passar o parâmetro
+signal
+
+# Chamando método passando um parâmetro
+signal("verde")
